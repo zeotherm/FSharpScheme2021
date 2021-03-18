ebfe310bfa5a9fccc5f8f1dacb468b062420c1fe
Matt Ford <zeotherm@gmail.com>
2021-03-17 20:34:03 -0400

    Basic calculator functionality present

--- FSharpScheme2021/Eval.fs
+++ FSharpScheme2021/Eval.fs
@@ -1,10 +1,37 @@
 ﻿module Eval
-
+open FSharpPlus
+open FSharpPlus.Data
+open FParsec
 open LispTypes
 
-let eval = 
+
+let rec unpackNum = 
+    function
+    | LispNumber v -> v
+    | LispString s -> match run pint64 s with
+                        | Success (v, _, _) -> v
+                        | Failure (err, _, _) -> 0L  // parse failed use 0
+    | LispList [n] -> unpackNum n
+    | _ -> 0L
+
+let numericBinOp op args = args |> List.map unpackNum |> List.reduce op |> LispNumber
+
+let primitives: Map<string, List<LispVal> -> LispVal> = 
+    Map.empty.
+        Add("+", numericBinOp (+)).
+        Add("-", numericBinOp (-)).
+        Add("*", numericBinOp (*)).
+        Add("/", numericBinOp (/)).
+        Add("mod", numericBinOp (%)).
+        Add("quotient", numericBinOp (/)).
+        Add("remainder", numericBinOp (fun a b -> (divRem a b) |> snd))
+
+let rec eval = 
     function
     | LispString _ as v -> v
     | LispNumber _ as v -> v
     | LispBool _ as v -> v
     | LispList [LispAtom "quote"; v ] -> v
+    | LispList (LispAtom func:: args) -> args |> List.map eval |> apply func
+and apply func args = 
+    Map.tryFind func primitives |> Option.map (fun f -> f args) |> Option.defaultWith (fun () -> LispBool false )
--- FSharpScheme2021/Program.fs
+++ FSharpScheme2021/Program.fs
@@ -3,36 +3,20 @@
 open NUnit.Framework
 open FsUnit
 open Parser
-
+open Eval
 [<Test>]
 let ``test hello``() = 
     5 + 1 |> should equal 6
 
 [<EntryPoint>]
 let main argv = 
-    let res1 = readExpr "\"this is a string\""
-    let res2 = readExpr "25"
-    let res3 = readExpr "symbol"
-    let res4 = readExpr "(symbol)"
-    printfn "res1\t%A\n" res1
-    printfn "res2\t%A\n" res2
-    printfn "res3\t%A\n" res3
-    printfn "res4\t%A\n" res4
 
-    let res5 = readExpr "(a test)"
-    let res6 = readExpr "(a (nested) test)"
-    let res7 = readExpr "(a (dotted . list) test)"
-    let res8 = readExpr "(a '(quoted (dotted . list)) test)"
-    let res9 = readExpr "(a '(imbalanced parens)"
-    printfn "res5\t%A\n" res5
-    printfn "res6\t%A\n" res6
-    printfn "res7\t%A\n" res7
-    printfn "res8\t%A\n" res8
-    printfn "res9\t%A\n" res9
-
-    let res10 = readExpr "(1 2 2)"
-    let res11 = readExpr "'(1 3 (\"this\" \"one\"))'"
-    printfn "res10\t%A\n" res10
-    printfn "res11\t%A\n" res11
-
+    let inp = [|"(+ 2 2)"|]
+    inp 
+    |> Array.tryHead 
+    |> Option.defaultValue "" 
+    |> readExpr 
+    |> eval 
+    |> (fun v -> v.ToString()) 
+    |> printfn "%s\n"
     0 // return an integer exit code
